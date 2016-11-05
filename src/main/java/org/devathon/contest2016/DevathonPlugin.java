75f499960a371eda21bea94f0b5db13f5c5cdf69
Mark Horn <markmediahorn@hotmail.com>
2016-11-05 14:57:20 +0100

    First commit

--- src/main/java/org/devathon/contest2016/DevathonPlugin.java
+++ src/main/java/org/devathon/contest2016/DevathonPlugin.java
@@ -1,12 +1,15 @@
 package org.devathon.contest2016;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DevathonPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // put your enable code here
+    	Bukkit.broadcastMessage("--------------------------------------");
+        Bukkit.broadcastMessage("Hello world!");
+        Bukkit.broadcastMessage("--------------------------------------");
     }
 
     @Override
